953b79f33dbfb72ad6ab8f90da110106e5701a72
gongupallyspandana30-jpg <gongupallyspandana30@gmail.com>
2026-08-12 15:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,7 @@ demo = gr.Interface(
     description = "Ask a question get an answer"
 
     )
-demo.launch(debug=True)
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
